7aaf19410eaf4de7ead812b2f65501556f58e05c
Jo-Yewon <yeawonjo@gmail.com>
2021-11-14 22:19:21 +0900

    Week 2-2 : Using state in Jetpack Compose

--- week 2-2-Using state in Jetpack Compose/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
@@ -16,6 +16,31 @@
 
 package com.codelabs.state.todo
 
+import com.codelabs.state.util.generateRandomTodoItem
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+/**
+ * 테스트가 더 편리해진 이유가 무엇일까?
+ * 기존에 ui 랑 비즈니스 로직이 섞여있는 경우에는, removeItem() 하면 거기에 UI 관련 로직도 들어있을거기 때문에...
+ * UI 없이는 테스트가 안됨.
+ * 근데 이제 됨.
+ */
 class TodoViewModelTest {
-    // TODO: Write tests
+
+    @Test
+    fun whenRemovingItem_updatesList() {
+        // before
+        val viewModel = TodoViewModel()
+        val item1 = generateRandomTodoItem()
+        val item2 = generateRandomTodoItem()
+        viewModel.addItem(item1)
+        viewModel.addItem(item2)
+
+        // during
+        viewModel.removeItem(item1)
+
+        // after
+        assertThat(viewModel.todoItems).isEqualTo(listOf(item2))
+    }
 }
